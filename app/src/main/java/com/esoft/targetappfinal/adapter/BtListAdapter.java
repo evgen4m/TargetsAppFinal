fde5e34bb291bc244e9d2fc7ebceb4a79d1b7ff7
Shutikov Evgeni <shutikov.e@yandex.ru>
2021-06-30 18:07:12 +0700

    fixed 1: Фикс поика устройств. Теперь устройства не дублируются

--- app/src/main/java/com/esoft/targetappfinal/adapter/BtListAdapter.java
+++ app/src/main/java/com/esoft/targetappfinal/adapter/BtListAdapter.java
@@ -1,6 +1,5 @@
 package com.esoft.targetappfinal.adapter;
 
-import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -9,15 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.esoft.targetappfinal.R;
 import com.esoft.targetappfinal.bluetooth.BtConstKt;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,14 +21,12 @@ public class BtListAdapter extends BaseAdapter {
     private static final int RESOURCE_LAYOUT = R.layout.list_item;
 
     private ArrayList<BluetoothDevice> bluetoothDevices;
-    private LayoutInflater layoutInflater;
     private SharedPreferences pref;
 
     private List<ViewHolder> lisViewHolders;
 
     public BtListAdapter(Context context, ArrayList<BluetoothDevice> bluetoothDevices) {
         this.bluetoothDevices = bluetoothDevices;
-        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         lisViewHolders = new ArrayList<>();
         pref = context.getSharedPreferences(BtConstKt.MY_PREF, Context.MODE_PRIVATE);
     }
@@ -62,17 +54,17 @@ public class BtListAdapter extends BaseAdapter {
         if(convertView == null) {
             viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(viewGroup.getContext()).inflate(RESOURCE_LAYOUT, null, false);
-            viewHolder.btName = convertView.findViewById(R.id.tv_name);
-            viewHolder.btAdress = convertView.findViewById(R.id.tv_address);
-            viewHolder.chBtSelected = convertView.findViewById(R.id.chConnection);
-            //viewHolder.bt_position = convertView.findViewById(R.id.tv_position);
             convertView.setTag(viewHolder);
             lisViewHolders.add(viewHolder);
         }else{
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        if(device != null){
+        viewHolder.btName = convertView.findViewById(R.id.tv_name);
+        viewHolder.btAdress = convertView.findViewById(R.id.tv_address);
+        viewHolder.chBtSelected = convertView.findViewById(R.id.chConnection);
+
+        if(bluetoothDevices != null){
             if(device.getName() == null) {
                 viewHolder.btName.setText("Неизвестное устройство!");
             }else {
@@ -87,24 +79,13 @@ public class BtListAdapter extends BaseAdapter {
             }
             viewHolder.chBtSelected.setChecked(true);
             savePref(i);
-            System.out.println(bluetoothDevices.get(i).getAddress());
+            System.out.println(device.getAddress());
         });
 
-        if(pref.getString(BtConstKt.MAC_KEY, "no bt selected").equals(bluetoothDevices.get(i).getAddress())){
+        if(pref.getString(BtConstKt.MAC_KEY, "no bt selected").equals(device.getAddress())){
             viewHolder.chBtSelected.setChecked(true);
         }
 
-       /* @SuppressLint("ViewHolder") View view = layoutInflater.inflate(RESOURCE_LAYOUT, viewGroup, false);
-        BluetoothDevice device = bluetoothDevices.get(i);
-        if(device != null){
-            if(device.getName() == null) {
-                ((TextView) view.findViewById(R.id.tv_name)).setText("Неизвестное устройство");
-            }else {
-                ((TextView) view.findViewById(R.id.tv_name)).setText(device.getName());
-            }
-            ((TextView) view.findViewById(R.id.tv_address)).setText(device.getAddress());
-            lisViewHolders.add()
-        }*/
         return convertView;
     }
 
